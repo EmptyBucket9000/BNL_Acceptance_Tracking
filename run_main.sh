550c5cd4772f6fe6abd8236d8efd369774926f94
Eric Schmidt <schmide2@my.erau.edu>
2017-01-31 10:16:44 -0700

    General updates

--- run_main.sh
+++ run_main.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-python3 main.py 1 group_19/
-python3 main.py 1 group_20/
-python3 main.py 1 group_21/
-python3 main.py 1 group_22/
-python3 main.py 1 group_23/
-python3 main.py 1 group_24/
+nohup python3 main.py 1 group_19/ &
+nohup python3 main.py 1 group_20/ &
+nohup python3 main.py 1 group_21/ &
+nohup python3 main.py 1 group_22/ &
+nohup python3 main.py 1 group_23/ &
+nohup python3 main.py 1 group_24/ &
+
 wait
-python3 main.py 1 group_19/
-python3 main.py 1 group_20/
-python3 main.py 1 group_21/
-python3 main.py 1 group_22/
-python3 main.py 1 group_23/
-python3 main.py 1 group_24/
+
+nohup python3 main.py 1 group_19/ &
+nohup python3 main.py 1 group_20/ &
+nohup python3 main.py 1 group_21/ &
+nohup python3 main.py 1 group_22/ &
+nohup python3 main.py 1 group_23/ &
+nohup python3 main.py 1 group_24/ &
